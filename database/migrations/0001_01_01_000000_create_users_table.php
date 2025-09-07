870a4532b154e079d3d9faf7c9a13349fc8890a7
Pedro Muñoz <munozpedro0800@gmail.com>
2025-09-07 14:06:25 -0300

    cambios para que ande test en backoffice/coincidencias

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
             $table->string('name');
             $table->string('cedula')->unique();
             $table->string('email')->unique();
-            $table->enum('rol', ['socio', 'admin'])->default('socio'); 
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->softDeletes();
