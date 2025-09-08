a0b9fa28b7bc8f2b24f24a7d3bebc6ad90b9883d
Pedro Muñoz <munozpedro0800@gmail.com>
2025-09-08 12:59:31 -0300

    API Usuario y Autenticación trabajada

--- app/Http/Controllers/SocioController.php
+++ app/Http/Controllers/SocioController.php
@@ -33,12 +33,55 @@ class SocioController extends Controller
         $socio->integrantes_familiares = $request->post("integrantes_familiares");
         $socio->fecha_ingreso = $request->post("fecha_ingreso");
         $socio->fecha_egreso = $request->post("fecha_egreso");
-        $password = $request->post('contraseña') ?? $request->post('cedula');
-        $socio->contraseña = Hash::make($password);
+        $password = $request->post('contraseña') ?? $request->post('cedula'); //Esto está para quitar
+        $socio->contraseña = Hash::make($password); 
 
         $socio->save();
 
         return $socio;
+    }
+
+      public function mostrar($id)
+    {
+        $socio = Socio::findOrFail($id);
+        return $socio;
+    }
+
+    
+    public function actualizar(Request $request, $id)
+    {
+        $socio = Socio::findOrFail($id);
+
+        if ($request->has('cedula'))                 $socio->cedula = $request->post("cedula");
+        if ($request->has('nombre'))                 $socio->nombre = $request->post("nombre");
+        if ($request->has('apellido'))               $socio->apellido = $request->post("apellido");
+        if ($request->has('telefono'))               $socio->telefono = $request->post("telefono");
+        if ($request->has('direccion'))              $socio->direccion = $request->post("direccion");
+        if ($request->has('fecha_nacimiento'))       $socio->fecha_nacimiento = $request->post("fecha_nacimiento");
+        if ($request->has('departamento'))           $socio->departamento = $request->post("departamento");
+        if ($request->has('ciudad'))                 $socio->ciudad = $request->post("ciudad");
+        if ($request->has('email'))                  $socio->email = $request->post("email");
+        if ($request->has('ingreso_mensual'))        $socio->ingreso_mensual = $request->post("ingreso_mensual");
+        if ($request->has('situacion_laboral'))      $socio->situacion_laboral = $request->post("situacion_laboral");
+        if ($request->has('integrantes_familiares')) $socio->integrantes_familiares = $request->post("integrantes_familiares");
+        if ($request->has('fecha_ingreso'))          $socio->fecha_ingreso = $request->post("fecha_ingreso");
+        if ($request->has('fecha_egreso'))           $socio->fecha_egreso = $request->post("fecha_egreso");
+        if ($request->has('estado'))                 $socio->estado = $request->post("estado");
+
+        
+        $socio->save();
+
+        return response()->json([
+            'message' => 'Socio actualizado',
+            'socio'   => $socio
+        ]);
+    }
+
+    public function eliminar($id)
+    {
+        $socio = Socio::findOrFail($id);
+        $socio->delete(); 
+        return response()->json(['message' => 'Socio eliminado']);
     }
 
     
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('cedula')->unique();
+            $table->string('cedula')->unique(); //tengo pendiente cambiar a int
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
