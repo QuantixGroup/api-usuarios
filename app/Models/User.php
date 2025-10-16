85c14a75dcdfcb07b88dd54e23a5055ed970551f
CrisLorenzo07 <sscristian77@gmail.com>
2025-10-16 18:39:11 -0300

    Se agrega el campo observacion para rechazar un recibo en la tabla pagos_mensuales

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,6 +23,7 @@ class User extends Authenticatable
     protected $table = 'users';
     protected $fillable = [
         'name',
+        'apellido',
         'cedula',
         'email',
         'password',
